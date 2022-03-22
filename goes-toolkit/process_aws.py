186beb853eefa3e5af892f38befd138aa0fb38fd
Helvecio Leal <helvecio.neto@inpe.br>
2022-03-22 11:32:59 -0300

    Corrections into scales of AWS

--- goes-toolkit/process_aws.py
+++ goes-toolkit/process_aws.py
@@ -2,6 +2,7 @@ import os
 import netCDF4
 from osgeo import gdal, osr
 import sys
+import numpy as np
 
 
 def process_aws(temp_output, output, timestamp, c_scan, s_scan):
@@ -10,7 +11,7 @@ def process_aws(temp_output, output, timestamp, c_scan, s_scan):
         var_name = 'CMI'
 
         output_dir = output + '/' + var_name + '/' + timestamp.strftime('%Y') + \
-            '/' + timestamp.strftime('%m') + '/' + timestamp.strftime('%d') + '/'
+            '/' + timestamp.strftime('%m') + '/'
 
         # Output file
         output_file = timestamp.strftime('%Y%m%d_%H%M.nc')
@@ -36,12 +37,6 @@ def process_aws(temp_output, output, timestamp, c_scan, s_scan):
         # Read the header metadata
         metadata = img.GetMetadata()
 
-        scale = float(metadata.get(var_name + '#scale_factor'))
-        offset = float(metadata.get(var_name + '#add_offset'))
-        undef = float(metadata.get(var_name + '#_FillValue'))
-
-        dtime = metadata.get('NC_GLOBAL#time_coverage_start')
-
         # long_name
         long_name = metadata.get(var_name + '#long_name')
         # coordinates
@@ -54,8 +49,6 @@ def process_aws(temp_output, output, timestamp, c_scan, s_scan):
         add_offset = metadata.get(var_name + '#add_offset')
         # valid_range
         valid_range = metadata.get(var_name + '#valid_range')
-        # missing_value
-        missing_value = metadata.get(var_name + '#missing_value')
         # _FillValue
         _FillValue = metadata.get(var_name + '#_FillValue')
 
@@ -82,12 +75,13 @@ def process_aws(temp_output, output, timestamp, c_scan, s_scan):
                   'dstSRS': target_prj,
                   'outputBounds': (bbox[0], bbox[3], bbox[2], bbox[1]),
                   'outputBoundsSRS': target_prj,
-                  'outputType': gdal.GDT_Float32, \
+                  #   'outputType': gdal.GDT, \
                   # 'srcNodata': undef, \
-                  'dstNodata': 'nan', \
+                  #   'dstNodata': 'nan', \
                   #   'xRes': 120, \
                   #   'yRes': 120, \
-                  'resampleAlg': gdal.GRA_NearestNeighbour}
+                  #   'resampleAlg': gdal.GRA_NearestNeighbour
+                  }
 
         # Write the reprojected file on disk
         gdal.Warp(temp_output, img, **kwargs)
@@ -95,51 +89,26 @@ def process_aws(temp_output, output, timestamp, c_scan, s_scan):
         # close the original file
         img.FlushCache()
 
-        # Read temp file
-        temp_cropped = gdal.Open(f'NETCDF:'+temp_output + ":Band1")
-
-        # # Load the data
-        array = temp_cropped.ReadAsArray(0, 0, temp_cropped.RasterXSize,
-                                         temp_cropped.RasterYSize)
-
-        # Apply the scale, offset
-        array = (array * scale * 100)
-
-        # Reproject the data
-        GeoT = temp_cropped.GetGeoTransform()
-        driver = gdal.GetDriverByName('netCDF')
-        raw = driver.Create(temp_output + '.temp1.nc', array.shape[1], array.shape[0], 1, gdal.GDT_Float32)
-        raw.SetGeoTransform(GeoT)
-        raw.GetRasterBand(1).WriteArray(array)
-
-        # Close the file
-        raw.FlushCache()
-
-        # close temp_cropped
-        temp_cropped.FlushCache()
-
-        del temp_cropped, raw
-
         # Add variables from metadata atributes
-        os.system("ncrename -h -O -v Band1," + var_name + " " + temp_output + '.temp1.nc')
-        os.system("ncatted -O -a long_name," + var_name + ",o,c,\""+str(long_name)+"\" "+str(temp_output) + '.temp1.nc')
-        os.system("ncatted -O -a coordinates," + var_name + ",o,c,\""+str(coordinates)+"\" "+str(temp_output) + '.temp1.nc')
-        os.system("ncatted -O -a units," + var_name + ",o,c,\""+str(units)+"\" "+str(temp_output) + '.temp1.nc')
-        os.system("ncatted -O -a scale_factor," + var_name + ",o,f,\""+str(scale_factor)+"\" "+str(temp_output) + '.temp1.nc')
-        os.system("ncatted -O -a add_offset," + var_name + ",o,c,\""+str(add_offset)+"\" "+str(temp_output) + '.temp1.nc')
-        os.system("ncatted -O -a valid_range," + var_name + ",o,c,\""+str(valid_range)+"\" "+str(temp_output) + '.temp1.nc')
-        os.system("ncatted -O -a _FillValue," + var_name + ",o,s,\""+str(_FillValue)+"\" "+str(temp_output) + '.temp1.nc')
+        os.system("ncrename -h -O -v Band1," + var_name + " " + temp_output)
+        os.system("ncatted -O -a long_name," + var_name + ",o,c,\""+str(long_name)+"\" "+str(temp_output))
+        os.system("ncatted -O -a coordinates," + var_name + ",o,c,\""+str(coordinates)+"\" "+str(temp_output))
+        os.system("ncatted -O -a units," + var_name + ",o,f,\""+str(units)+"\" "+str(temp_output))
+        os.system("ncatted -O -a scale_factor," + var_name + ",o,f,\""+str(scale_factor)+"\" "+str(temp_output))
+        os.system("ncatted -O -a add_offset," + var_name + ",o,f,\""+str(add_offset)+"\" "+str(temp_output))
+        os.system("ncatted -O -a valid_range," + var_name + ",o,c,\""+str(valid_range)+"\" "+str(temp_output))
+        os.system("ncatted -O -a _FillValue," + var_name + ",o,s,\""+str(_FillValue)+"\" "+str(temp_output))
 
         # Add global attributes
-        os.system("ncatted -O -a processed,global,o,c,\" by: Helvecio B. L. Neto (helvecioblneto@gmail.com)\" "+str(temp_output) + '.temp1.nc')
+        os.system("ncatted -O -a processed,global,o,c,\" by: Helvecio B. L. Neto (helvecioblneto@gmail.com)\" "+str(temp_output))
 
         # Add global_dict to attributes
         for k in global_dict.keys():
-            os.system("ncatted -O -a " + k[10:] + ",global,o,c,\""+str(global_dict[k])+"\" "+str(temp_output) + '.temp1.nc')
+            os.system("ncatted -O -a " + k[10:] + ",global,o,c,\""+str(global_dict[k])+"\" "+str(temp_output))
 
         # Add variables
         # Read netCDF using nc
-        nc = netCDF4.Dataset(temp_output + '.temp1.nc', 'r+')
+        nc = netCDF4.Dataset(temp_output, 'r+')
 
         # Add variable satlat
         nc.createDimension('satlat', 1)
@@ -155,10 +124,13 @@ def process_aws(temp_output, output, timestamp, c_scan, s_scan):
         nc.variables['satlon'].long_name = 'Satellite Longitude'
         nc.variables['satlon'].units = 'degrees_east'
 
+        # Convert julian to string char
+        julian_day_char = netCDF4.stringtochar(np.array([str(timestamp.timetuple().tm_yday)], 'S4'))
+
         # Add variable julian_day
-        nc.createDimension('julian_day', 1)
-        nc.createVariable('julian_day', 'f4', ('julian_day',))
-        nc.variables['julian_day'][:] = timestamp.timetuple().tm_yday
+        nc.createDimension('julian_day', 4)
+        nc.createVariable('julian_day', 'S1', ('julian_day',))
+        nc.variables['julian_day'][:] = julian_day_char
         nc.variables['julian_day'].long_name = 'Julian day'
         nc.variables['julian_day'].units = 'day'
         nc.variables['julian_day'].comment = str(timestamp.timetuple().tm_yday)
@@ -166,24 +138,21 @@ def process_aws(temp_output, output, timestamp, c_scan, s_scan):
         # Transform timestamp to hour of day and minute of hour
         hour_of_day = timestamp.strftime('%H')
         minute_of_hour = timestamp.strftime('%M')
-        time_of_day = int(hour_of_day + minute_of_hour)
+        time_of_day = str(hour_of_day + minute_of_hour)
+
+        # Convert time_of_day to string char
+        time_of_day_char = netCDF4.stringtochar(np.array([str(time_of_day)], 'S4'))
 
         # Add variable time_of_day
-        nc.createDimension('time_of_day', 1)
-        nc.createVariable('time_of_day', 'f4', ('time_of_day',))
-        nc.variables['time_of_day'][:] = time_of_day
+        nc.createDimension('time_of_day', 4)
+        nc.createVariable('time_of_day', 'S1', ('time_of_day',))
+        nc.variables['time_of_day'][:] = time_of_day_char
         nc.variables['time_of_day'].long_name = 'Time of day'
         nc.variables['time_of_day'].units = 'hour and minute'
         nc.variables['time_of_day'].comment = str(time_of_day)
 
         # Close the file
         nc.close()
-
-        # Remove local file
-        os.remove(temp_output)
-
-        # Rename file
-        os.rename(temp_output + '.temp1.nc', temp_output)
 
         # Move file to output directory
         os.rename(temp_output, output_dir + output_file)
